87b721c5d07034640c7d2bae5d0dc61284d4e60f
tpm92 <36012530+tpm92@users.noreply.github.com>
2018-04-09 18:01:55 -0400

    blocks
    object that gives boolean if the laser reflects or refracts

--- objects/block.py
+++ objects/block.py
@@ -1,5 +1,5 @@
 
-class Block:
+class Block(object):
     '''
     A generic block for lazor.  We make this extendable so that it can be
     defined as either:
@@ -8,10 +8,20 @@ class Block:
         (b) Opaque block - Absorbs the laser
         (c) See-Through block - Both reflects and lets light pass
     '''
-    def __init__(self):
-        '''
-        Difficulty 1
+    def __init__(self,n):
+        self.num = n
+        self.laser_int(n)
+        self.reflect
+        self.refract
+    def laser_int(self,num): #how it interacts with a laser hitting it
+        Reflect = False
+        Refract = False
+        if num in (2,3):
+            Reflect=True
+            if num == 2:
+                Refract = True
+        self.reflect = Reflect
+        self.refract = Refract
+            
+block = Block(2)  
 
-        DONT FORGET TO COMMENT!
-        '''
-        pass
