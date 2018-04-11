3e314a5724f7b50f27814426820a494ef928ad1a
tpm92 <36012530+tpm92@users.noreply.github.com>
2018-04-11 16:39:56 -0400

    final block using string

--- objects/block.py
+++ objects/block.py
@@ -19,10 +19,12 @@ class Block(object):
     def laser_int(self,num): #how it interacts with a laser hitting it
         Cont = False #laser continues in direction
         Reflect = False # laser is reflected
-        if num in (2,3):
+        if num in ('2','3'):
             Reflect=True
-        if num in (2,0,1):
+        if num in ('2','0','1'):
             Cont = True
         self.reflect = Reflect
         self.continues = Cont
             
+A = Block('2')
+print(A.reflect)
