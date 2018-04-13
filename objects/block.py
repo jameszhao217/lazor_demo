86b6297384d242391a0188f8659df84b51d3aa8e
tpm92 <36012530+tpm92@users.noreply.github.com>
2018-04-13 10:21:07 -0400

    kdhkjeh
    comments added

--- objects/block.py
+++ objects/block.py
@@ -1,24 +1,16 @@
 
 class Block(object):
-    '''
-    A generic block for lazor.  We make this extendable so that it can be
-    defined as either:
-
-        (a) Reflecting block - Only reflects the laser
-        (b) Opaque block - Absorbs the laser
-        (c) See-Through block - Both reflects and lets light pass
-    '''
+   
     def __init__(self,n):
-        # n is integer decribing block type
-        # 0 available, 1 unavailable, 2 refract,3reflect,4opaque
-        
+        # n a list of an integer decribing block type
+        # 0 available, 1 unavailable, 2 refract,3reflect,4opaque  
         self.num = n
         self.laser_int(n)
         self.continues
         self.reflect
     def laser_int(self,num): #how it interacts with a laser hitting it
-        Cont = False #laser continues in direction
-        Reflect = False # laser is reflected
+        Cont = False #laser does not continues in direction
+        Reflect = False # laser does not reflect
         if num in ('2','3'):
             Reflect=True
         if num in ('2','0','1'):
--- objects/point.py
+++ objects/point.py
@@ -1,17 +1,11 @@
 import numpy as np
 class Point(object):
-    '''
-    The Point.  This object desribes the points for which we want the laser
-    light to intersect.
-    '''
+    
     def __init__(self, coordinates):
-        '''
-        Difficulty 1
-
-        DONT FORGET TO COMMENT!
-        '''
+        # stores point coordinates
         self.coord1 = coordinates
     def check_intersection(self,test1):
+        # checks for laser intersection of coordinates
         if self.coord1[0] == test1[0] and self.coord1[1] == test1[1]:
             return True
         else:
